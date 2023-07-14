7b280d62c5c49d184a567c7c1d9ffee318c9e072
luk3rF7 <lkzeron@gmail.com>
2023-07-14 14:26:52 -0300

    list com objetos

--- 03_advancer/reactAvancado/src/components/listRender.jsx
+++ 03_advancer/reactAvancado/src/components/listRender.jsx
@@ -3,14 +3,26 @@ import { useState } from 'react';
 const ListRender = () => {
 
   const [ list ] = useState(["lucas","user_1","user_2"])
+  const [ user ] = useState([
+    {id:1,name: 'user_2',age: 18},
+    {id:2,name:'user_3',age: 20},
+    {id:3,name:'user_4',age:15}
+  ])
   return (
     <div>
       <h1> Lista Render</h1>
       <hr />
       <ul>
-        {list.map((item) => (
+        {list.map((item,i) => (
           // eslint-disable-next-line react/jsx-key
-          <li >{item}</li>
+          <li key={i} >{item}</li>
+        ))}
+      </ul>
+      <h2> List User</h2>
+      <ul>
+        {user.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age}</li>
         ))}
       </ul>
     </div>
