08975e7ee6f39aeeb54cad9042febda5288e15b7
luk3rF7 <lkzeron@gmail.com>
2023-07-14 14:40:37 -0300

    excluindo user

--- 03_advancer/reactAvancado/src/components/listRender.jsx
+++ 03_advancer/reactAvancado/src/components/listRender.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 
 const ListRender = () => {
-
+ //hooks
   const [ list ] = useState(["lucas","user_1","user_2"])
-  const [ user ] = useState([
+  const [ users,setUsers ] = useState([
     {id:1,name: 'user_2',age: 18},
     {id:2,name:'user_3',age: 20},
     {id:3,name:'user_4',age:15}
   ])
+
+    //function
+    const deletRandom = () =>{
+      const randomNumber = Math.floor(Math.random() * 4)
+      setUsers((prevUsers) => {
+        return prevUsers.filter((user) => randomNumber !== user.id)
+      })
+    }
   return (
     <div>
       <h1> Lista Render</h1>
@@ -20,11 +28,12 @@ const ListRender = () => {
       </ul>
       <h2> List User</h2>
       <ul>
-        {user.map((user) => (
+        {users.map((user) => (
           <li key={user.id}>
             {user.name} - {user.age}</li>
         ))}
       </ul>
+      <button onClick={deletRandom}>Delete from user</button>
     </div>
   )
 }
