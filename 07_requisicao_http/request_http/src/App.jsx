55e84b55373abcd7c34c812fe5ef1bb7e5d289ed
luk3rF7 <118012621+Luk3rF7@users.noreply.github.com>
2023-07-15 05:13:24 -0300

    get http

--- 07_requisicao_http/request_http/src/App.jsx
+++ 07_requisicao_http/request_http/src/App.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
+const url = "http://localhost:3000/products"
 function App() {
- 
+  //guarda os dados
+ const [products,setProducts] = useState([])
+ //resgastando dados
+  useEffect(() =>{
+ try{
+  // eslint-disable-next-line no-inner-declarations
+  async function fetchData() {
+    const res = await fetch(url)
+    const data = await res.json()
+    setProducts(data)
+  } 
+  fetchData()
+ }catch(e){
+  console.error(e)
+ }
+  },[])
+  console.log(products)
   return (
     <>
-       <div className={App}>
+       <div >
       <h1>Lista de Produtos</h1>
       </div>
     </>
