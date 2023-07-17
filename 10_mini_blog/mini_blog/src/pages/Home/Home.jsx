22bb0c68b5d4cd4d0a9bf5c719bc63154e5faa42
luk3rF7 <118012621+Luk3rF7@users.noreply.github.com>
2023-07-17 00:57:40 -0300

    Home e login

--- 10_mini_blog/mini_blog/src/pages/Home/Home.jsx
+++ 10_mini_blog/mini_blog/src/pages/Home/Home.jsx
@@ -1,19 +1,55 @@
-//style Css
+// css
 import styles from './Home.module.css'
-import blog1 from '../../assets/svg/Blog1.svg'
-import blog3 from '../../assets/svg/Blog3.svg'
-/* import blog3 from '../../assets/svg/Blog3.svg' */
+// react
+import { useState } from "react";
 
-export default function Home() {
+// components
+import PostDetail from "../../components/post/PostDetail";
+import { useFetchDocuments } from './../../hook/useFetchDocuments';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Home = () => {
+  const { documents: posts, loading } = useFetchDocuments("posts");
+
+  const navigate = useNavigate();
+
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (query) {
+      return navigate(`/search?q=${query}`);
+    }
+  };
+
+  console.log(loading);
+
   return (
-    
-    <div className={styles.container}>
-      <div>
-      <img src={blog3} alt="" className={styles.img} />
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div className="post-list">
+        {loading && <p>Carregando...</p>}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
       </div>
-    <div >
-      <img src={blog1} alt="" className={styles.img2} />
-    </div>
     </div>
   );
-}
+};
+
+export default Home;
--- 10_mini_blog/mini_blog/src/pages/Login/Login.jsx
+++ 10_mini_blog/mini_blog/src/pages/Login/Login.jsx
@@ -1,21 +1,74 @@
 import style from './Login.module.css'
+//import
+import { useEffect, useState } from "react";
+import { useAuthentication } from '../../hook/useAuthentication'
+
 export default function Login() {
+  // * inicio: instancia e utilização do hooks
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { login, error: authError, loading } = useAuthentication();
+
+  // * funcoes: 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError("");
+
+    const user = {
+      email,
+      password,
+    };
+
+    const res = await login(user);
+
+    console.log(res);
+  };
+  // * pegando de api
+  useEffect(() => {
+    console.log(authError);
+    if (authError) {
+      setError(authError);
+    }
+  }, [authError]);
+
+ // * renderizar e componentizar
   return (
     <div className={style.container_login}>
       <h1>Login</h1>
-      <form className={style.form}>
+      <form 
+      onSubmit={handleSubmit}
+      className={style.form}>
         <label>
           <span>E-mail:</span>
-          <input type="email" />
+          <input 
+          type="email"
+          name="email"
+          required
+          placeholder='E-mail do Usuário'
+          onChangeCapture={(e) => setEmail(e.target.value)}
+          value={email}
+          />
         </label>
         <label>
           <span>Senha:</span>
-          <input type="password" />
+          <input
+           type="password"
+           name="password"
+           required
+           placeholder='Insira a senha'
+           onChange={(e) => setPassword(e.target.value)}
+           value={password}
+           />
         </label>
           Esqueceu a senha? 
-        <label>
-        <button>Login</button>
-        </label>
+        {/* condição para visualiza botao :*/} 
+         {!loading && <button>Login</button>}
+         {loading && ( 
+        <button>Aguarde...</button>
+        )}
+        {error && <p className='error'>{error}</p>}
       </form>
     </div>
   )
