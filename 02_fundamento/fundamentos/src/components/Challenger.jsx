9dd27b1feb7e8c09327f001ab47aef8343de8142
luk3rF7 <118012621+Luk3rF7@users.noreply.github.com>
2023-07-17 04:18:51 -0300

    .

--- 02_fundamento/fundamentos/src/components/Challenger.jsx
+++ 02_fundamento/fundamentos/src/components/Challenger.jsx
@@ -2,14 +2,16 @@
 export default  function Challenger(){
 const a = 10;
 const b = 20;
-
+  const handleSum = () =>{
+    console.log(a+b)
+  }
   
   
   return (
     <div>
       <h1>valor A: {a}</h1>
       <h1>valor B :{b}</h1>
-      <button onClick={(() => console.log(a+b) )}>clique aqui</button>
+      <button onClick={handleSum}>clique aqui</button>
       </div>
   )
 }
